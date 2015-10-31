cb57594491dc0e04641680bd26a7400eacc918f0
David Chavarria <dvchavarriam@gmail.com>
2015-10-31 16:56:09 -0600

    Upload Inicial

---
+++ WebApplication1/Actions/Sesion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Actions
+{
+    public class Sesion
+    {
+        public bool IniciarSesion(string usuario, string clave)
+        {
+            usuario = usuario.ToLower();
+            
+            if (usuario == "wawila" && clave == "123")
+                return true;
+
+            return false;
+        }
+
+    }
+}
---
+++ WebApplication1/Controllers/LogInController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Actions;
+
+
+namespace WebApplication1.Controllers
+{
+    public class LogInController : Controller
+    {
+        // GET: LogIn
+        public ActionResult Index()
+        {
+            return View("LogIn");
+        }
+
+        public ActionResult LogIn()
+        {
+            Sesion li = new Sesion();
+            String usuario = "";
+            String clave = "";
+            
+            if (li.IniciarSesion(usuario, clave))
+                return View("Dashboard");
+
+            return View("LogIn");
+        }
+    }
+}
