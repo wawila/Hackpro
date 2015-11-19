33ef63750c755659efb69cf5886340cdef29dd61
David Chavarria <dvchavarriam@gmail.com>
2015-11-19 13:23:30 -0600

    Avance Formulario y Dashboard

--- WebApplication1/Controllers/DashboardController.cs
+++ WebApplication1/Controllers/DashboardController.cs
@@ -9,9 +9,15 @@ namespace WebApplication1.Controllers
     public class DashboardController : Controller
     {
         // GET: Dashboard
-        public ActionResult Index()
+        public ActionResult Dashboard()
         {
             return View("Dashboard");
         }
+
+        public ActionResult EventForm()
+        {
+            return View("EventForm");
+        }
+
     }
 }
---
+++ WebApplication1/Models/EventModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class EventModel
+    {
+        [Required(ErrorMessage ="Ingresar   Correcto")] 
+        public string tbl_evento_nombre { get; set; }
+
+        [Required(ErrorMessage = "Ingresar   Correcto")]
+        public string tbl_evento_desc { get; set; }
+
+        [Required(ErrorMessage = "Ingresar   Correcto")]
+        public string tbl_evento_lugar { get; set; }
+
+        [Required(ErrorMessage = "Ingresar   Correcto")]
+        public int tbl_evento_duracion { get; set; }
+
+        [Required(ErrorMessage = "Ingresar   Correcto")]
+        public DateTime tbl_evento_fecha_inicio { get; set; }
+        
+        [Required(ErrorMessage = "Ingresar   Correcto")]
+        public decimal tbl_evento_presupuesto { get; set; }
+
+        [Required(ErrorMessage = "Ingresar   Correcto")]
+        public string tbl_evento_url { get; set; }
+
+        [Required(ErrorMessage = "Ingresar   Correcto")]
+        public bool tbl_evento_cal_jurado { get; set; }
+
+        [Required(ErrorMessage = "Ingresar   Correcto")]
+        public bool tbl_evento_cal_pueblo { get; set; }
+
+        [Required(ErrorMessage = "Ingresar   Correcto")]
+        public decimal tbl_evento_precio { get; set; }
+    }
+}
